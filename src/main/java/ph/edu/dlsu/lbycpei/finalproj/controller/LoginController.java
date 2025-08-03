23e40f94d610aea83841b743564dceb6f18aae6c
Nirah24 <145692234+Nirah24@users.noreply.github.com>
2025-08-03 18:07:16 +0800

    whats inside the proj(GUIs not done): - the login screen - the main layout(left sidebar and header)
    keep in mind that after the user logs in, the first thing they would see is the calendar
    
    to-dos:
    - fix gui
    - add features
    - username: s, pw: 1

--- src/main/java/ph/edu/dlsu/lbycpei/finalproj/controller/LoginController.java
+++ src/main/java/ph/edu/dlsu/lbycpei/finalproj/controller/LoginController.java
@@ -1,7 +1,9 @@
 package ph.edu.dlsu.lbycpei.finalproj.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -21,16 +23,36 @@ public class LoginController {
     private Label statusLabel;
 
     @FXML
+    private void handleSignupLink(ActionEvent event) {
+        try {
+            Parent signupRoot = FXMLLoader.load(getClass().getResource("/ph/edu/dlsu/lbycpei/finalproj/sign-up.fxml"));
+
+            //
+            Scene scene = new Scene(signupRoot);
+            scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML
     private void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
+
 
         // TEMPORARY: Hardcoded credentials
         if ("s".equals(username) && "1".equals(password)) {
             try {
                 // Load the MainLayout
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/ph/edu/dlsu/lbycpei/finalproj/main-layout.fxml"));
-                System.out.println("FXML URL: " + getClass().getResource("/ph/edu/dlsu/lbycpei/finalproj/main-layout.fxml"));
+            //    System.out.println("FXML URL: " + getClass().getResource("/ph/edu/dlsu/lbycpei/finalproj/main-layout.fxml"));
                 Parent root = loader.load();
 //                Scene scene = new Scene(root, 1440, 960);   //
 //                stage.setScene(scene);
